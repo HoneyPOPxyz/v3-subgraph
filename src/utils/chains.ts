cf5d7850af15d9bcc8a3d4580eaa09c590f327f7
0xsecp256r1 <0xsecp256r1@proton.me>
2025-04-02 22:44:55 +1100

    fix

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -462,7 +462,7 @@ export function getSubgraphConfig(): SubgraphConfig {
     return {
       factoryAddress: '0x1511bb1fc5b38c83f9e0034be2c14f1a0038d93a',
       stablecoinWrappedNativePoolAddress: '0x94446ffc0b32a4df47a2e2fcacb70714f33fd694', // USDC/WETH 0.01% pool
-      stablecoinIsToken0: true,
+      stablecoinIsToken0: false,
       wrappedNativeAddress: '0x5300000000000000000000000000000000000004', // WETH
       minimumNativeLocked: BigDecimal.fromString('1'),
       stablecoinAddresses: [
